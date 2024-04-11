67db76c69db59c6854c3e739b229d7b1d3b6be8a
joaov <joaovitorlf@gmail.com>
2024-04-11 15:22:05 +0100

    shop

--- src/Components/filter/filter.js
+++ src/Components/filter/filter.js
@@ -2,9 +2,40 @@ import {React, useState} from "react";
 import { Link } from "react-router-dom";
 
 export function Filter(){
-    return(
+    const {filterParams, setParams} = useState("")
+    function OrderDropdown() {
+        const [selectedOption, setSelectedOption] = useState('');
+
+        const handleSelectChange = (event) => {
+            setSelectedOption(event.target.value);
+        };
+
+        return (
+            <div>
+                <select id="dropdown" value={selectedOption} onChange={handleSelectChange}>
+                    <option value="sort={},descs">Classificação ↓</option>
+                    <option value="ClassificaçãoAsc">Classificação ↑</option>
+                    <option value="PreçoAsc">Preço ↑</option>
+                    <option value="PreçoDesc">Classificação ↓</option>
+                </select>
+            </div>
+        );
+    }
+
+    function FilterInput() {
+        const {filterString, updateFilter} = useState("")
+        function updateSearch(e){
+            updateFilter(e.target.value)
+        }
+        return(
+        <input value={filterString} onChange={updateFilter}></input>
+        )
+    }
+
+    return (
         <div>
-            <p>fe</p>
+            <OrderDropdown/>
+            <FilterInput/>
         </div>
     )
 }
