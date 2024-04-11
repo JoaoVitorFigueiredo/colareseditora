f322a37108c76e47fb01d9d5bd6bbf2191624fe9
joaov <joaovitorlf@gmail.com>
2024-04-11 13:18:16 +0100

    começo do filtro de pesquisa (Muito incompleto aind

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,6 +1,28 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+import {BookThumbnail} from "../../Components/book-thumbnail";
 
 export function Search(){
-    return <p>1</p>
+    let { title } = useParams()
+    const [books, setBook] = useState([])
+
+
+    useEffect(() => {
+        fetchBooks();
+    },[])
+    const fetchBooks = async () => {
+        try {
+            const response = await fetch(`http://localhost:3030/books/?title=${title}`,{method:"GET"})
+            const bookData = await response.json();
+            setBook(bookData[0])
+        }
+        catch (error){
+            console.error(`Error fetching data: ${error}`) // Era engraçado fazer isso daqui enviar pra uma página de erro.
+        }
+    }
+    return (
+        <div>
+            {books.map(book => BookThumbnail(book.id,book.title,book.publishedDate,book.price,book.status,book.authors,book.score,book.thumbnailUrl))}
+        </div>
+    )
 }
