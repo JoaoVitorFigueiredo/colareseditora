7a95424ca7ffa340f14650ca703c7e64aadb6a7b
João Chung <joaochungferreira@gmail.com>
2024-04-12 11:29:56 +0100

    Updated search bar visual.

--- src/Components/navbar/navbar.js
+++ src/Components/navbar/navbar.js
@@ -2,64 +2,66 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
-
-
 function NavBar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleMenu = () => setIsMenuOpen(prevState => !prevState);
 
-    const [searchString, setSearchString] = useState('')
+    const [searchString, setSearchString] = useState('');
 
-    function updateSearch(e){
-        setSearchString(e.target.value)
+    function updateSearch(e) {
+        setSearchString(e.target.value);
     }
-    
-    return (
 
+    return (
         <nav className="navbar">
             <div className="nav-container">
-            <Link exact to="/" className="nav-logo">
-                <img src="https://www.transparentpng.com/thumb/books/blue-book-png-icon--an1pZY.png" alt="J&J Books Logo" className="logo-image"/>
-                <span>J&J Books</span>
-            </Link>
+                <Link exact to="/" className="nav-logo">
+                    <img src="https://www.transparentpng.com/thumb/books/blue-book-png-icon--an1pZY.png" alt="J&J Books Logo" className="logo-image"/>
+                    <span>J&J Books</span>
+                </Link>
 
-            <div className="nav-toggle" onClick={toggleMenu}>
-                <div className={isMenuOpen ? "nav-icon open" : "nav-icon"}></div>
-            </div>
+                <div className="nav-search-container">
+                    <input 
+                        type="text"
+                        placeholder="Pesquisa..."
+                        className="nav-search-input"
+                        value={searchString}
+                        onChange={updateSearch}
+                    />
+                    <Link to={`/search/${searchString}`} className="nav-search-icon">
+                        <i className="fas fa-search"></i>
+                    </Link>
+                </div>
 
-            <ul className={isMenuOpen ? "nav-menu active" : "nav-menu"}>
-                <li className="nav-item">
-                <input value={searchString} onChange={updateSearch}></input>
-                </li>
-                <li className="nav-item" onClick={toggleMenu}>
-                <Link to={`/search/${searchString}`} activeClassName="active" className="nav-links">
-                    Pesquisar
-                </Link>
-                </li>
-                <li className="nav-item" onClick={toggleMenu}>
-                <Link exact to="/" activeClassName="active" className="nav-links">
-                    Home
-                </Link>
-                </li>
-                <li className="nav-item" onClick={toggleMenu}>
-                <Link exact to="/shop" activeClassName="active" className="nav-links">
-                    Loja
-                </Link>
-                </li>
-                <li className="nav-item" onClick={toggleMenu}>
-                <Link exact to="/sales" activeClassName="active" className="nav-links">
-                    Promoções
-                </Link>
-                </li>
-                <li className="nav-item" onClick={toggleMenu}>
-                <Link exact to="/cart" activeClassName="active" className="nav-links">
-                <i className="fas fa-shopping-cart"></i>
-                </Link>
-                </li>
-            </ul>
+                <div className="nav-toggle" onClick={toggleMenu}>
+                    <div className={isMenuOpen ? "nav-icon open" : "nav-icon"}></div>
+                </div>
+
+                <ul className={isMenuOpen ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item" onClick={toggleMenu}>
+                        <Link exact to="/" activeClassName="active" className="nav-links">
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item" onClick={toggleMenu}>
+                        <Link exact to="/shop" activeClassName="active" className="nav-links">
+                            Loja
+                        </Link>
+                    </li>
+                    <li className="nav-item" onClick={toggleMenu}>
+                        <Link exact to="/sales" activeClassName="active" className="nav-links">
+                            Promoções
+                        </Link>
+                    </li>
+                    <li className="nav-item" onClick={toggleMenu}>
+                        <Link exact to="/cart" activeClassName="active" className="nav-links">
+                            <i className="fas fa-shopping-cart"></i>
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
 }
-  
+
 export default NavBar;
