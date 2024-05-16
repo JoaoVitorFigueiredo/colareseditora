25ebacf29dfb8d9280f82e1dfa4f6c27f0fb2da6
joaov <joaovitorlf@gmail.com>
2024-05-16 15:49:58 +0100

    delete book

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,10 @@ from flask import Flask
 from pymongo import MongoClient
 from flask_cors import CORS
 from dotenv import load_dotenv
+from functools import wraps
 import os
+
+
 
 load_dotenv()
 
@@ -18,4 +21,16 @@ db = client["library_db"]
 books_collection = db["books"]
 users_collection = db["users"]
 
+def token_required(func):
+ @wraps(func)
+ def decorated(*args, **kwargs):
+  
+  return func(*args,**kwargs)
+
+ return decorated
+
+
 from routes import books, users
+
+
+
--- backend/routes/books.py
+++ backend/routes/books.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request, abort
-from app import app, books_collection
+from app import app, books_collection , token_required
+
 
 @app.route("/api/v1/allbooks", methods=["GET"])
 def get_books():
@@ -13,10 +14,10 @@ def get_books():
     # Formato JSON
     return jsonify(books)  # Lista de livros como resposta JSON
 
+
 # Definição da rota para listar os livros com paginação
 @app.route("/api/v1/books", methods=["GET"])
 def list_books():
-
     # Obter parâmetros de consulta para paginação
     # Default page = 1 e limit = 10 (pesquisa sem parâmetros de consulta)
     page = int(request.args.get("page", 1))
@@ -29,24 +30,25 @@ def list_books():
     books = list(books_collection.find({}).skip(start_index).limit(limit))
 
     for book in books:
-        book["_id"] = str(book["_id"])  
+        book["_id"] = str(book["_id"])
 
     return jsonify(books)
+
 
 # Definição da rota para procurar os livros pelo ID (inteiro)
 @app.route("/api/v1/books/<string:id>", methods=["GET"])
 def get_book_by_id(id):
-
     # Procurar o livro pelo campo 'id'
     book = books_collection.find_one({"id": id})
 
     if book:
 
-        book["_id"] = str(book["_id"])  
-        return jsonify(book)  
-    
+        book["_id"] = str(book["_id"])
+        return jsonify(book)
+
     else:
         abort(404)
+
 
 @app.route("/api/v1/books/featured", methods=["GET"])
 def get_featured_books():
@@ -54,9 +56,10 @@ def get_featured_books():
     featured_books = list(books_collection.find().sort([("score", -1), ("price", 1)]).limit(5))
 
     for book in featured_books:
-        book["_id"] = str(book["_id"])  
+        book["_id"] = str(book["_id"])
 
     return jsonify(featured_books)
+
 
 @app.route("/api/v1/books/total", methods=["GET"])
 def get_total_books():
@@ -64,3 +67,15 @@ def get_total_books():
     total_books = books_collection.count_documents({})
 
     return jsonify({"total_books": total_books})
+
+
+@app.route("/api/v1/books/<string:id>", methods=["DELETE"])
+@token_required
+def delete_book_by_id(id):
+    result = books_collection.delete_one({"id": id})
+
+
+    if result.deleted_count == 0:
+        return jsonify({"message": "Error", "status": "book not found"}), 404
+
+    return jsonify({"message": "Success", "status": "book deleted"}), 200
