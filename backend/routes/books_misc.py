41a788d98c46ed99920cf7fac03bc2fa8d6871f3
joaov <joaovitorlf@gmail.com>
2024-05-26 12:19:38 +0100

    Toques finais nas respostas

--- backend/routes/books_misc.py
+++ backend/routes/books_misc.py
@@ -21,24 +21,34 @@ def add_new_books():
     books_data = request.get_json()
 
     if not isinstance(books_data, list):
-        return jsonify({"message": "A list of books is required"}), 400
+        return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "A list of books is required"}), 400
 
     for book_data in books_data:
         if "_id" in book_data:
-            return jsonify({"message": "field '_id' is reserved",
+            return jsonify({"code": "400",
+                        "status": "Bad Request",
+                            "message": "field '_id' is reserved",
                             "invalid book": book_data}), 400
         if not "id" in book_data:
-            return jsonify({"message": "id is required",
+            return jsonify({"code": "400",
+                        "status": "Bad Request",
+                            "message": "id is required",
                             "invalid book": book_data}), 400
         if not isinstance(book_data["id"], int):
-            return jsonify({"message": "id must be numeric",
+            return jsonify({"code": "400",
+                        "status": "Bad Request",
+                            "message": "id must be numeric",
                             "invalid book": book_data}), 400
         elif books_collection.find_one({"id": book_data["id"]}):
-            return jsonify({"message": "id must be unique",
+            return jsonify({"code": "400",
+                        "status": "Bad Request",
+                            "message": "id must be unique",
                             "invalid book": book_data}), 400
 
     books_collection.insert_many(books_data)
-    return jsonify({'message': 'Success', 'status': 'Books added successfully'}), 200
+    return jsonify({'status': 'Success', 'message': 'Books added successfully'}), 200
 
 
 @app.route("/api/v1/books/<int:id>", methods=["PUT"])
@@ -47,12 +57,14 @@ def update_book(id):
     book_data = request.get_json()
 
     if "id" in book_data:
-        return jsonify({"message": "Changing the id of the book is not allowed",
-                        "invalid book": book_data}), 400
+        return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "Changing the id of the book is not allowed"}), 400
 
     if "_id" in book_data:
-        return jsonify({"message": "field '_id' is reserved",
-                        "invalid book": book_data}), 400
+        return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "field '_id' is reserved"}), 400
 
     book_data["id"] = id
 
@@ -66,22 +78,35 @@ def save_cart():
     cart_data = request.get_json()
 
     if "total" not in cart_data or "volume" not in cart_data or "books" not in cart_data:
-        return jsonify({"message": "The shopping cart must contain 'total' , 'volume' and 'total'"}), 400
+        return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "The shopping cart must contain 'total , 'volume' and 'total'"}), 400
 
     if not (isinstance(cart_data["total"], float) or isinstance(cart_data["total"], int)):
-        return jsonify({"message": "The total value must be numeric' , 'volume' and 'total'"}), 400
+        return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "The total value must be numeric"}), 400
 
     if not isinstance(cart_data["volume"], int):
-        return jsonify({"message": "The number of numbers (volume) must be an integer' , 'volume' and 'total'"}), 400
+        return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "The number of books (volume) must be an integer"}), 400
 
     if not isinstance(cart_data["books"], list):
-        return jsonify({"message": "The books in the cart must be in a list"}), 400
+        return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "The books in the cart must be in a list"}), 400
 
     for book in cart_data["books"]:
         if not "id" in book:
-            return jsonify({"message": "Couldn't find the book's id"}), 400
+            return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": "Couldn't find the book's id"}), 400
         if not books_collection.find_one({"id": book["ID"]}):
-            return jsonify({"message": f"The book ({book['ID']}), don't exists."}), 400
+            return jsonify({"code": "400",
+                        "status": "Bad Request",
+                        "message": f"The book with the id {book['ID']}, don't exists."}), 400
 
     cart_collection.insert_one(cart_data)
     return jsonify({'message': 'Success', 'status': 'Cart saved'}), 200
+
